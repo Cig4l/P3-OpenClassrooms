3ad96a62894e16df3cf67b4117641f621b41c0c5
Cigale <coralieruiz.dev@gmail.com>
2023-11-20 17:37:43 +0100

    Fetch les travaux

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,1 +1,21 @@
-fetch("http://localhost:5678/api/works");
+//
+//                              RECUPERATION DES TRAVAUX
+//
+
+let listeTravaux;
+
+// envoi de requête GET
+ listeTravaux = fetch("http://localhost:5678/api/works")
+    // then => requête traitée en asynchrone
+    // .json : convertit requête en JSON
+    .then(response => response.json())
+    .then(data => {
+        // Pending Promise
+        listeTravaux = data;
+        // console.log(data);
+    })
+    .catch(error => {
+        console.error(error);
+    });
+
+
