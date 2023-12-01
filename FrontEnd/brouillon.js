cd3b5c5d7c02f1ae12ebd2aa084260d6509aa064
Cigale <coralieruiz.dev@gmail.com>
2023-12-01 15:09:29 +0100

    changement de scope pour LOGIN et LOGIN_JSON : global + changement d evaleur dans event

--- FrontEnd/brouillon.js
+++ FrontEnd/brouillon.js
@@ -1,42 +1,17 @@
-const LOGIN = {
-    "email": "sophie.bluel@test.tld",
-    "password": "S0phie"
-}
-
-const LOGIN_JSON = JSON.stringify(LOGIN);
-
-// const WRONG_ID = document.getElementById("wrong-id");
-
-async function postLogin () {
-    const response = await fetch("http://localhost:5678/api/users/login", {
-        method: "POST",
-        headers: {"Content-Type": "application/json"},
-        body: LOGIN_JSON
-    });
-    if(!response.ok) {
-      throw new Error("Mauvais email ou mot de passe")
-    }
-    const json = await response.json();
-    return json;
-}
-
-async function login () {
-    // Récupération de l'userId + token
-    const login = await postLogin();
-
-    const admin = {
-        "userId": await login.userId,
-        "token": await login.token
+let LOGIN = {
+    "email": "",
+    "password": ""
     }
 
-    if(admin.token !== undefined){
-        console.log(admin);
-        return admin;
+let LOGIN_JSON = "";
+
+LOGIN = {
+    "email": "@",
+    "password": "pw"
     }
-    else{
-        console.log("red");
-    }
-}
+
+LOGIN_JSON = JSON.stringify(LOGIN);
+
+console.log(LOGIN);
+
 console.log(LOGIN_JSON);
-
-login();
--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -3,9 +3,28 @@
 //     "password": "S0phie"
 // }
 
-// const LOGIN_JSON = JSON.stringify(LOGIN);
+// Document Object Model
+
+let emailLogin = document.getElementById("email-login");
+let valueEmailLogin = emailLogin.value;
+
+let mdpLogin = document.getElementById("mdp-login");
+let valueMdpLogin = mdpLogin.value;
+
+let connexionButton = document.getElementById("connexion-button");
 
 const WRONG_ID = document.getElementById("wrong-id");
+
+// Variables stockant les infos de login
+
+let LOGIN = {
+    "email": "",
+    "password": ""
+    }
+
+let LOGIN_JSON = "";
+
+// FONCTION FETCH POST LOGIN
 
 async function postLogin () {
     const response = await fetch("http://localhost:5678/api/users/login", {
@@ -20,47 +39,38 @@ async function postLogin () {
     return json;
 }
 
+// FONCTION POUR LOGIN
+
 async function login () {
     // Récupération de l'userId + token
     const login = await postLogin();
+    console.log(login);
 
     const admin = {
         "userId": await login.userId,
         "token": await login.token
     }
+    console.log(admin);
 
     if(admin.token !== undefined){
-        console.log(admin);
         return admin;
     }
-    else{
-        console.log("red");
-    }
+    WRONG_ID.innerText = "Oups";
 }
 
-console.log(login());
-
-// SE CONNECTER
-
-let emailLogin = document.getElementById("email-login");
-let valueEmailLogin = emailLogin.value;
-
-let mdpLogin = document.getElementById("mdp-login");
-let valueMdpLogin = mdpLogin.value;
-
-let connexionButton = document.getElementById("connexion-button");
+// EVENT POUR SE CONNECTER
 
 connexionButton.addEventListener("click", function (event) {
     // Annule le comportement par défaut du bouton de connexion
     event.preventDefault();
-    
-    const LOGIN = {
-        "email": emailLogin.value,
-        "password": mdpLogin.value
+
+    LOGIN = {
+    "email": emailLogin.value,
+    "password": mdpLogin.value
     }
-    
-    const LOGIN_JSON = JSON.stringify(LOGIN);
+
+    LOGIN_JSON = JSON.stringify(LOGIN);
     console.log(LOGIN_JSON);
 
-    login()
+    login();
 })
