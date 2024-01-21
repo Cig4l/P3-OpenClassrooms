576d30f839afff71fcc9698e7a87e5bb1df88c9e
Cigale <coralieruiz.dev@gmail.com>
2024-01-21 15:18:23 +0100

    suppression de brouillon.js

--- FrontEnd/brouillon.js
+++
@@ -1,8 +0,0 @@
-// else{
-    // submitMessage.textContent = "Soumission réussie !"
-    // submitMessage.style.color = "green";
-    // ajouterContainer.style.padding = "19px";
-    // ajouterInterface.style.display = "flex";
-    // displayImage.style.display = "none";
-    // displayGallery(0)
-// }
